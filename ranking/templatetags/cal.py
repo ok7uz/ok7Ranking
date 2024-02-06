1ce22b602edee159100e6dfcc217c480968390d7
obloev <komronbekobloev@gmail.com>
2024-02-07 01:10:48 +0500

    optimized

--- ranking/templatetags/cal.py
+++ ranking/templatetags/cal.py
@@ -1,17 +1,18 @@
 from django import template
-from ranking.functions import *
-
+from ranking.functions import calculate
+from decimal import Decimal
 register = template.Library()
 
 @register.simple_tag(name="calc")
-def function(match, team, g1, g2):
+def calculate_points(match, team, g1, g2):
+    team1_points = match.team1.points
+    team2_points = match.team2.points
     if team == 1:
-        return calculate(match.team1.points, match.team2.points, g1, g2, match.tournament.coef, match.is_knockout)
-    return calculate(match.team2.points, match.team1.points, g1, g2, match.tournament.coef, match.is_knockout)
+        return calculate(team1_points, team2_points, g1, g2, match.tournament.coef, match.is_knockout)
+    return calculate(team2_points, team1_points, g1, g2, match.tournament.coef, match.is_knockout)
 
 @register.simple_tag(name="win_p")
-def win_p(points1: Decimal, points2: Decimal):
+def winning_probability(points1: Decimal, points2: Decimal):
     dr = points1 - points2
     ex = 1 / (pow(10, -dr / 600) + 1)
-
-    return round(ex * 100) 
+    return round(ex * 100)
